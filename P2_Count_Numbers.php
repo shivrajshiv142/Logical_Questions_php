9f2ec399eb788657f01e988dc9534b6e43fd6604
shivrajshiv142 <144521120+shivrajshiv142@users.noreply.github.com>
2024-06-09 13:51:46 +0530

    add echo

--- P2_Count_Numbers.php
+++ P2_Count_Numbers.php
@@ -1,3 +1,22 @@
 <?php
-echo " count number list";
-?>
+  $number = 150;
+   
+ if ($number >= 100) {
+    echo"error";
+ } else if ($number >= 90){
+    echo"A";
+ }else if($number >= 80){
+     echo "B";   
+    }elseif($number >= 70){
+        echo "C";
+    }elseif ($number >= 60) {
+        echo"D";
+    }elseif($number >= 50){
+        echo"E";
+    }elseif ($number >= 40) {
+        echo "F";
+    }elseif ($number <= 30) {
+     echo"Fail";
+    }
+
+  ?>
